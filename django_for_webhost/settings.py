990e6308eb69e9b81d78f4f4993cc91ffad304c0
Albert <37086349+albertiq@users.noreply.github.com>
2020-07-17 20:47:07 +0500

    Update settings.py

--- django_for_webhost/settings.py
+++ django_for_webhost/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-2-@m()m)0qu03bzv8p-0d&!d1o+6fb$^wdwom!df%@du^mi^m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
